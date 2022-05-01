ad4d0bcc3c3063561013341416eb57c1d26d778f
Andreas Poulsen <andreas_poulsen@hotmail.com>
2022-05-01 12:54:24 +0200

    Finished mini-project

--- Mini-Project/gpu_mandelbrot.py
+++ Mini-Project/gpu_mandelbrot.py
@@ -8,7 +8,8 @@ import numpy as np
 import pyopencl as cl
 
 
-def plot_gpu(height: int, width: int, wrkGrp_size: int, MAX_ITER: np.uint16 = np.uint16(80)) -> None:
+def plot_gpu(height: int, width: int, device: int,
+             MAX_ITER: np.uint16 = np.uint16(80)) -> None:
     """
         Plots the mandelbrot set calculated with an opencl kernel.
 
@@ -17,8 +18,8 @@ def plot_gpu(height: int, width: int, wrkGrp_size: int, MAX_ITER: np.uint16 = np
             Height component of the resolution. Determines the number of rows in the 2D mandelbrot set.
         width: int
             Width component of the resolution. Determines the number of columns in the 2D mandelbrot set.
-        wrkGrp_size: int
-            Size of the pyopencl work group.
+        device: int
+            Compute device.
         MAX_ITER: int
             Maximum number of iterations.
 
@@ -30,12 +31,12 @@ def plot_gpu(height: int, width: int, wrkGrp_size: int, MAX_ITER: np.uint16 = np
 
     # Create the host side data and a empty array to hold the result.
     a = np.linspace(-2, 1, height)
-    b = np.linspace(-1.5, 1.5, width) * 1j
-    C_host = np.ravel(a + b[:, np.newaxis]).astype(np.complex128)
+    b = np.linspace(-1.5, 1.5, width)*1j
+    C_host = np.ravel(a + b[:, np.newaxis]).astype(np.complex64)
     n_host = np.zeros((height, width), dtype=np.uint16)
 
     # Create the context (containing platform and device information) and command queue.
-    context = cl.create_some_context()
+    context = cl.create_some_context(answers=[device])
     cmd_queue = cl.CommandQueue(context)
 
     # Create a device side read-only memory buffer and copy the data from "hostbuf" into it.
@@ -45,46 +46,41 @@ def plot_gpu(height: int, width: int, wrkGrp_size: int, MAX_ITER: np.uint16 = np
     n_device = cl.Buffer(context, mf.WRITE_ONLY, n_host.nbytes)
 
     kernel = """
+    #include <pyopencl-complex.h>
     __kernel void mandelbrot(
-        __global const float2 *C,
+        __global const cfloat_t *C,
         __global       ushort *n,
                  const ushort MAX_ITER)
     {
         int gid = get_global_id(0);
-        float nreal, real = 0;
-        float imag = 0;
-
+        cfloat_t Z;
+        Z.real = 0;
+        Z.imag = 0;
         n[gid] = 0;
 
-        for(int i = 0; i < MAX_ITER; i++)
+        while(Z.real*Z.real + Z.imag*Z.imag <= 4 && n[gid] < MAX_ITER)
         {
-            nreal = real * real - imag * imag + C[gid].x;
-            imag = 2 * real * imag + C[gid].y;
-            real = nreal;
-
-            if(real*real + imag*imag >= 4)
-            {
-                n[gid] = i;
-                break;
-            }
+            cfloat_t Z_mul = cfloat_mul(Z,Z);
+            Z = cfloat_add(Z_mul, C[gid]);
+            n[gid]++;
         }
-        
     }
     """
 
     prog = cl.Program(context, kernel).build()
 
-    prog.mandelbrot(cmd_queue, n_host.shape, None,
+    prog.mandelbrot(cmd_queue, C_host.shape, None,
                     C_device, n_device, MAX_ITER)
 
     cl.enqueue_copy(cmd_queue, n_host, n_device)
 
     end = timer()
-    plt.figtext(0.5, 0.01, "Time to run: {:.2f} seconds".format(end-start), ha="center",
-                fontsize=14, bbox={"facecolor": "white", "alpha": 0.5, "pad": 4})
+    plt.figtext(0.5, 0.01, "Time to run: {:.2f} seconds".format(end-start),
+                ha="center", fontsize=14, bbox={"facecolor": "white",
+                "alpha": 0.5, "pad": 4})
     plt.imshow(n_host, cmap='magma')
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_gpu(1500, 1500, 1024)
+    plot_gpu(1500, 1500, 1)
